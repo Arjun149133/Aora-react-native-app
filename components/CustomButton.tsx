f1be6b4650fa345718c55d6ff1f6385ce305ffb2
Arjun <arjun149133@gmail.com>
2024-05-16 01:34:53 +0530

    Landing screen

---
+++ components/CustomButton.tsx
@@ -0,0 +1,43 @@
+import {
+  View,
+  Text,
+  TouchableOpacityComponent,
+  TouchableOpacity,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
+} from "react-native";
+import React from "react";
+
+interface CustomButtonProps {
+  title: string;
+  handlePress: () => void;
+  containerStyles?: string;
+  textStyles?: StyleProp<TextStyle>;
+  isLoading?: boolean;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-secondary-100 rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={` text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
